43b5dec1089b041471c8e719d09404200d2d60e4
kira-coding <kirubelabeben@gmail.com>
2025-02-20 22:48:42 +0300

    toast

--- components/sign-up.tsx
+++ components/sign-up.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { signUp } from "@/lib/auth-client";
-import { toast } from "sonner";
+
 import { useRouter } from "next/navigation";
 import { Loader2 } from "lucide-react";
 
